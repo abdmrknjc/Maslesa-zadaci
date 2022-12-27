397f06a1a7d64f9cde5f05d44a6d70096f782742
abdmrknjc <mrkonjicabdullah08@gmail.com>
2022-12-27 11:40:07 +0100

    007.start

---
+++ 007/007/007.cpp
@@ -0,0 +1,8 @@
+/*
+Napisati program koji ce ucitati prirodni broj n<=10, a zatim n cijelih brojeva.
+Za svaki broj treba ispitati radi li se o broju koji je 'dobar'. 
+Zatim sve dobre brojeve prebaciti u novi niz te ispisati njegove elemente i velicinu.
+Koristiti zasebne funkcije za unos i provjeru je li broj dobar.
+Napomena: Broj je je 'dobar' ako je svaka njegova cifra veca od zbira cifara koji se nalaze desno od nje.
+Na primjer, 9620 je 'dobar' jer je 2>0, 6>2+0, 9>6+2+0. Drugi primjeri: 8421, 95210, 732, 621, 852 itd.
+*/
